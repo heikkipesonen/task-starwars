6ed1523e45da2dbf40f65892471c2c549ad10f6f
heikki pesonen <heikki.pesonen@sok.fi>
2024-02-05 17:48:27 +0200

    feat(app): add marker on map click

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import './App.css'
+import mapboxgl from 'mapbox-gl'
 import * as stylex from '@stylexjs/stylex'
 import { Map } from './components/map'
 import { useSecretData } from './services/use-secret-data'
 
 const App: FC = () => {
+  const [myPosition, setMyPosition] = useState<mapboxgl.Marker | null>(null)
   const markers = useSecretData()
+
+  const handleMapClick = (e: mapboxgl.MapMouseEvent) => 
+    setMyPosition((x) => {      
+      // a marker could be mutated, but this is the way #mandalorian
+      if (x) {
+        x.remove()
+      }
+
+      return new mapboxgl.Marker({ color: 'green' }).setLngLat(e.lngLat)
+    })
+  
 
   return (
     <main {...stylex.props(styles.main)}>
-      <Map markers={[...markers]} onClick={console.log} />
+      <Map
+        markers={[...markers, ...(myPosition ? [myPosition] : [])]}
+        onClick={handleMapClick}
+      />
     </main>
   )
 }
