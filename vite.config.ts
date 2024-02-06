3372ce732262cfb03e462136ec30ce85fdf24423
heikki pesonen <heikki.pesonen@sok.fi>
2024-02-06 11:13:22 +0200

    chore(ci): add github deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import styleX from "vite-plugin-stylex";
+import styleX from 'vite-plugin-stylex'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/task-starwars/',
   plugins: [react(), styleX()],
 })
